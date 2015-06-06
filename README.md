675cf6f0f0e7ab78441097de7e645d7ae6712517
Bratie Fanut <bratiefanut@gmail.com>
2015-06-06 19:26:48 +0300

    v1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Unity-timer-script
 Timer - Unity C# script + example project
+
+1. Attach the script Timer.cs to any game object you want;
+2. Drag and drop a UI Text component in the label field.
+3. Magic
---
+++ Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float minutes, seconds, miliseconds;
+
+    private float intTime;
+    
+    public Text label;
+
+    // Update is called once per frame
+    void Update()
+    {
+        intTime += Time.deltaTime;
+
+        minutes = intTime / 60;
+        seconds = intTime % 60;
+        miliseconds = Time.deltaTime * 1000;
+        miliseconds = miliseconds % 1000;
+
+        if (label != null)
+        {
+            label.text = /*"Time is " +*/ (int)minutes + ":" + (int)seconds;
+        }
+    }
+}
